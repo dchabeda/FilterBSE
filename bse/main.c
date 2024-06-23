56a1ecaca067f97a3edfb94104faf5398881bb6d
Daniel Chabeda <daniel_chabeda@berkeley.edu>
2024-06-23 18:23:30 -0400

    setting the qp basis functions

--- bse/main.c
+++ bse/main.c
@@ -120,11 +120,11 @@ int main(int argc, char *argv[]){
     ist.eval_hole_idxs = realloc(ist.eval_hole_idxs, ist.n_holes * sizeof(long));
     ist.eval_elec_idxs = realloc(ist.eval_hole_idxs, ist.n_elecs * sizeof(long));
 
-    print("\neval_hole_idxs:\n");
+    printf("\neval_hole_idxs:\n");
     for (i = 0; i < ist.n_holes; i++){
         printf("%ld\n", ist.eval_hole_idxs[i]);
     }
-    print("\neval_elec_idxs:\n");
+    printf("\neval_elec_idxs:\n");
     for (i = 0; i < ist.n_elecs; i++){
         printf("%ld\n", ist.eval_elec_idxs[i]);
     }
